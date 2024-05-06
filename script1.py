122d9188ddd30e20ec3ca4957ddb7f4bdf567c93
Charles baudinet <ch.baudinet@gmail.com>
2024-05-06 10:58:01 +0200

    c3

--- script1.py
+++ script1.py
@@ -1,12 +1,13 @@
 """This file is cool"""
 
+import random
+
+
 def disgustingcode():
     """
     Blablabla
     :return:
     """
-
-    import random
 
     # This function prints out a random number of stars to the console
     for _ in range(random.randint(1, 10)):
