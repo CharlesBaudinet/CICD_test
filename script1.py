fff8d45dbbe5068d6a3346b22956114a369422ab
Baudinet <ch.baudinet@gmail.com>
2024-05-06 10:46:13 +0200

    c1

---
+++ script1.py
@@ -0,0 +1,9 @@
+def disgustingcode():
+    import random, os
+
+    # This function prints out a random number of stars to the console
+    for _ in range(random.randint(1, 10)):
+        print("*", end=" ")
+    print("\n")
+    print("This is disgusting code")
+    print("You can't even read it")
